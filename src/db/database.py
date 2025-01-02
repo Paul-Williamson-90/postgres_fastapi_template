8972a77f5c8aaa15bb94149317f0d97e0158ce12
Paul Williamson <84777418+Paul-Williamson-90@users.noreply.github.com>
2025-01-02 18:49:52 +0000

    chore: code comments

--- src/db/database.py
+++ src/db/database.py
@@ -10,6 +10,7 @@ DB_USER=os.getenv("DB_USER")
 DB_PASSWORD=os.getenv("DB_PASSWORD")
 DB_NAME=os.getenv("DB_NAME")
 
+# NOTE: Switch 'db' to your localhost if running locally
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@db:5432/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
